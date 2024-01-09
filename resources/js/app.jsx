62587be0c6aace23a7f30d528f590563a4760c98
fangplay <fangplay_2014@hotmail.com>
2024-01-09 20:16:53 +0700

    Project Setup

---
+++ resources/js/app.jsx
@@ -0,0 +1,25 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import CompaniesIndex from "./Pages/Shop/Index";
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+const root = createRoot(document.getElementById('app'));
+root.render(<CompaniesIndex />);
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
